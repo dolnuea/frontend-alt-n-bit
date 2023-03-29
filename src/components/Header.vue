87ac3d23fc412f8d5e41b1e53ec8523dcc4c4c99
Luna Dagci <ldagci@albany.edu>
2023-03-28 21:07:33 -0400

    Build a simple page for demo

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <v-toolbar
+      color="black"
+      dark
+      prominent
+      image="https://png.pngtree.com/thumb_back/fh260/background/20210828/pngtree-digital-code-encryption-dark-blue-technology-background-image_770047.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Alternative N-bit Key Data Encryption for Block Ciphers</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <Header msg="Welcome to Your Vue.js App" />
+    <p>Upload file here</p>
+    <v-file-input label="File input"></v-file-input>
+
+    <p>OR.... enter text below!</p>
+
+    <v-container fluid>
+    <v-textarea
+      label="Enter ciphertext/plaintext text here"
+      no-resize
+      rows="10"
+      :model-value="value"
+    ></v-textarea>
+  </v-container>
+
+    <v-btn style="margin: 20px;">Encrypt</v-btn>
+    <v-btn>Decrypt</v-btn>
+
+    <v-row justify="center" style="margin: 50px;">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card title="Results" rounded min-height="500"></v-card>
+      </v-card>
+    </v-col>
+  </v-row>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+
+p {
+  margin: 40px 20px 20px;
+}
+</style>
