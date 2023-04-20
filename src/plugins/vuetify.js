d2a6c0429fd3e4fdbcb203db069461d5510e59e8
Luna Dagci <ldagci@albany.edu>
2023-04-20 18:59:53 -0400

    Trying to get mdi

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,14 @@
 import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default createVuetify({
+  components,
+  directives,
   icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    }
+    iconfont: 'mdi',
   },
 })
